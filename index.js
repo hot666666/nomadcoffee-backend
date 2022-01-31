f06efe2fb7d19f1dc7d0b12008b1594348b328f5
hot666666 <hotcs6@naver.com>
2022-01-31 18:36:21 +0900

    day 8 of 43 + .

--- index.js
+++ index.js
@@ -16,3 +16,58 @@ const server = new ApolloServer({
 server.listen().then(({ url }) => {
   console.log(`🚀 Server ready at ${url}`);
 });
+
+/*
+const express = require("express");
+const { ApolloServer, gql } = require("apollo-server-express");
+const { GraphQLUpload, graphqlUploadExpress } = require("graphql-upload");
+const { finished } = require("stream/promises");
+
+const typeDefs = gql`
+  scalar Upload
+  type File {
+    filename: String!
+  }
+  type Query {
+    otherFields: Boolean!
+  }
+  type Mutation {
+    singleUpload(file: Upload!): File!
+  }
+`;
+
+const resolvers = {
+  Upload: GraphQLUpload,
+  Mutation: {
+    singleUpload: async (_, { file }) => {
+      const { createReadStream, filename } = await file;
+      //const newFilename = `${loggedInUser.id}-${Date.now()}-${filename}`;
+      const stream = createReadStream();
+      const out = require("fs").createWriteStream(
+        process.cwd() + "/uploads/" + filename
+      ); //newFilename);
+      console.log(process.cwd() + "/uploads/" + filename);
+      stream.pipe(out);
+      await finished(out);
+
+      return { filename };
+    },
+  },
+};
+
+async function startServer() {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+  });
+
+  await server.start();
+  const app = express();
+  app.use(graphqlUploadExpress());
+  server.applyMiddleware({ app });
+  app.use("/static", express.static("uploads"));
+  await new Promise((r) => app.listen({ port: 4000 }, r));
+  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
+}
+startServer();
+*/
--- users/user.typeDefs.js
+++ users/user.typeDefs.js
@@ -10,6 +10,12 @@ export default gql`
     avatarURL: String
     location: String
     githubUsername: String
+    following: [User]
+    followers: [User]
+    totalFollowing: Int!
+    totalFollowers: Int!
+    createdAt: String!
+    updatedAt: String!
   }
   type Query {
     _empty: String
